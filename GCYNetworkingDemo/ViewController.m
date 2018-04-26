c73484d35dfa0b50313940a706c7b9ac80f92384
gaochongyang <gaochongyang@enmonster.com>
2018-04-26 17:18:56 +0800

    Fix

--- GCYNetworkingDemo/ViewController.m
+++ GCYNetworkingDemo/ViewController.m
@@ -18,9 +18,19 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    UIButton *button = [[UIButton alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
+    button.backgroundColor = [UIColor redColor];
+    [button setTitle:@"click" forState:UIControlStateNormal];
+    [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [button addTarget:self action:@selector(buttonClick) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:button];
+
+}
+- (void)buttonClick {
     [self createTestOneTask];
 
 }
+
 - (void)createTestOneTask {
     [[GCYTestOneTask sharedInstance] get:@"GCYBase" parameters:nil success:^(NSURLSessionDataTask *task, id responseObject) {
         NSLog(@"%@", responseObject);
